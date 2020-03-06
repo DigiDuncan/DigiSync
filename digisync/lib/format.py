c67d49e1ab54de3ff9d8efd2d552ed1f5975a0e8
DigiDuncan <digiduncan@gmail.com>
2020-03-06 17:56:38 -0500

    Move constants out of functions for speed.

--- digisync/lib/format.py
+++ digisync/lib/format.py
@@ -1,9 +1,15 @@
+MILLISECONDS_PER_SECOND = 1000
+MILLISECONDS_PER_MINUTE = MILLISECONDS_PER_SECOND * 60
+MILLISECONDS_PER_HOUR = MILLISECONDS_PER_MINUTE * 60
+MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24
+MILLISECONDS_PER_YEAR = MILLISECONDS_PER_DAY * 365
+
+prefixes = ["K", "M", "G", "T", "P"]
+
 def formatByteSize(byteamount: int):
     # Technically this is returning it in kibibytes, etc.,
     # but claims it's in kilobytes etc. Windows does this
     # so it's fine.
-
-    prefixes = ["K", "M", "G", "T", "P"]
 
     if byteamount < 1024:
         return f"{byteamount}B"
@@ -16,11 +22,6 @@ def formatByteSize(byteamount: int):
     return f"{byteamount:.3f}EB"
 
 def formatTimeDelta(totalSeconds, millisecondAccuracy = True):
-    MILLISECONDS_PER_SECOND = 1000
-    MILLISECONDS_PER_MINUTE = MILLISECONDS_PER_SECOND * 60
-    MILLISECONDS_PER_HOUR = MILLISECONDS_PER_MINUTE * 60
-    MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24
-    MILLISECONDS_PER_YEAR = MILLISECONDS_PER_DAY * 365
 
     milliseconds = int(totalSeconds * 1000)
     totalmilliseconds = milliseconds

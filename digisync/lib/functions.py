e30192fffff03f7cae0f9a163af837a402c9d059
DigiDuncan <digiduncan@gmail.com>
2020-03-08 17:58:43 -0400

    Add old stuff from DPNSync.

--- digisync/lib/functions.py
+++ digisync/lib/functions.py
@@ -1,7 +1,14 @@
-import xxhash
-import digiformatter
 import time
 import sys
+import ctypes
+import platform
+import os
+import re
+
+from pathlib import Path
+
+import xxhash
+import digiformatter
 
 from digisync.lib.format import formatByteSize, formatTimeDelta
 
@@ -58,3 +65,28 @@ def readINI(ini):
     lines = inif.readlines()
     # TODO: Do stuff...
     inif.close()
+
+
+# Get the users free space. Should be regardless of platform?
+def getFreeSpace(dirname):
+    if platform.system() == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(dirname), None, None, ctypes.pointer(free_bytes))
+        return free_bytes.value
+    else:
+        st = os.statvfs(dirname)
+        return st.f_bavail * st.f_frsize
+
+
+def getFileDirectory(path):
+    # [::-1] is slice syntax for reversing a string.
+    folder = path[::-1]
+    folder = re.sub(r'.*?\/', '', folder, 1)
+    folder = folder[::-1]
+    return folder
+
+
+def makeFileList(path):
+    p = Path(path)
+    filelist = list(p.glob('**/*.*'))
+    filelist = [str(e) + '\n' for e in filelist]
